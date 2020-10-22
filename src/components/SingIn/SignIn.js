7d1cb156aef7013d98d47ebbd828f06b2e2f6d88
Bishesh <bishesh.sunam@gmail.com>
2020-10-22 17:08:41 +1300

    deploy

--- src/components/SingIn/SignIn.js
+++ src/components/SingIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component{
     }
 
     onSubmitSignin =()=>{
-        fetch('http://localhost:5000/signin', {
+        fetch('https://frozen-temple-66405.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify({
